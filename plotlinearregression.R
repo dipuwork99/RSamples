1cc62e998de41b952eceddcf3386d94a41d5e8e5
dipuwork99 <dipu99.work@gmail.com>
2022-03-16 08:15:54 +0000

    Create plotlinearregression.R
    Sample file for R

---
+++ plotlinearregression.R
@@ -0,0 +1,8 @@
+samplex <- sample(1:100, 100, replace = TRUE)
+
+sampley <- samplex + rnorm(100, sd = 4)
+mydf <- data.frame(x = samplex, y = sampley)
+plot(sampley ~ samplex, data = mydf)
+model <- lm(sampley ~ samplex, data = mydf)
+abline(model, col = "red")
+summary(model)
